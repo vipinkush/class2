06b20d584928048a42bfadef11bd02fbe6993460
vipin kushwaha <kushwahavipin2002@gmail.com>
2026-04-24 18:59:00 +0530

    learn frontend

---
+++ p.py
@@ -0,0 +1,11 @@
+num = int(input("enter a no"))
+if num > 0 :
+ a = int(input("enter a no"))
+ b = int(input("enter a no"))
+ print(a*b)
+elif num <0 :
+ c = int(input("enter a no"))
+ print(c*c*c)
+
+elif num == 0:
+ print("neutral")
